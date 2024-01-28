eb782844b12996bf020f8cf8145dda995f6b5d36
DyosTheNerd <irondwarf@gmx.de>
2024-01-28 10:56:58 +0100

    less padding

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,5 @@ import type { AppProps } from 'next/app'
 import './globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return <main className="flex min-h-screen flex-col items-center justify-between p-24"><Component {...pageProps} /></main>
+    return <main className="flex min-h-screen flex-col items-center justify-between"><Component {...pageProps} /></main>
 }
