11407eeeb269bc7f9fee44f830813a89b2d3c860
DyosTheNerd <irondwarf@gmx.de>
2024-02-18 15:48:46 +0100

    correcting env var for url

--- src/pages/api/server/setup.ts
+++ src/pages/api/server/setup.ts
@@ -2,7 +2,7 @@ import * as qrcode from "qrcode";
 
 // current url on vercel from env variable
 
-const baseUrl = process.env.NEXT_PUBLIC_URL  ? `https://${process.env.NEXT_PUBLIC_URL }` : "http://localhost:3000"
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL }` : "http://localhost:3000"
 
 export default async function GET (req: any,res: any ) {
 
